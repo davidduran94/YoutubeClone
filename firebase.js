413052614babb56f99cde1bda122beca135f72a8
David Duran <davidduran94@gmail.com>
2020-08-11 18:16:10 -0500

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAUQroPovNAf1bp0LkIQPeSSQahqzJXChQ",
+  authDomain: "clone-9f62f.firebaseapp.com",
+  databaseURL: "https://clone-9f62f.firebaseio.com",
+  projectId: "clone-9f62f",
+  storageBucket: "clone-9f62f.appspot.com",
+  messagingSenderId: "288119659719",
+  appId: "1:288119659719:web:a2c7e273765af4c13732e2",
+  measurementId: "G-FEV4TP1W5P"
+};
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    //BEM class naming convection
+    <div className="App">
+      <h1>Lets build a youtube clone</h1>
+    </div>
+  );
+}
+
+export default App;
